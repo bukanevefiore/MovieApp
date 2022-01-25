ef9b386f841c78c65ae2aa24450c6e3c91ad7784
Melis Nur KILIÇ <melisnurklc0@gmail.com>
2022-01-25 16:31:30 +0300

    package installations & styles parameters created

---
+++ movieApp/src/App.js
@@ -0,0 +1,14 @@
+// json-server --watch MOVIES.json
+
+import React from 'react';
+
+export default function App() {
+  return (
+    <SafeAreaView >
+     <Text></Text>
+    </SafeAreaView>
+  );
+}
+
+
+export default App;
